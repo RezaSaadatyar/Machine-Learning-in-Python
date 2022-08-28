2ead1a8eb65eb3ba4e7b37db587972dd46798041
Reza Saadatyar <96347878+RezaSaadatyar@users.noreply.github.com>
2022-08-28 23:27:22 +0430

    Add files via upload

---
+++ Code/Main.py
@@ -0,0 +1,62 @@
+# ==========================================================================
+# ============================ Machine Learning  ============================
+# ====================== Presented by: Reza Saadatyar  =====================
+# =================== E-mail: Reza.Saadatyar92@gmail.com  ==================
+# ============================  2022-2023 ==================================
+# The program will run automatically when you run code/file Main.py, and you do not need to run any of the other codes.
+
+# ===================================== Importing the required Libraries =============================
+from sklearn import datasets
+from Filtering import filtering
+from Clustering import clustering
+from Preparing_data import preparing_data
+from Normalize import normalize_data
+from Classification import classification
+from Plot_clustering import plot_cluster
+from Feature_Selection import featureselection
+from Feature_Extraction import feature_extraction
+from Output_Training_Test_Network import output_network
+# ============================================Step 1: Preparing the data ==============================================
+iris = datasets.load_iris()
+Data = iris.data
+Labels = iris.target
+# Data, Labels = datasets.make_blobs(150, 4, centers=4, random_state=0)
+preparing_data(Data, Labels)
+# ======================================== Step 2: Filtering & Data scaling ============================================
+# Data = filtering(Data, F_low=5, F_high=10, Order=3, Fs=50, btype='bandpass')      # btype:'low', 'high', 'bandpass', 'bandstop'
+# Data = normalize_data(Data, Type_Normalize='MinMaxScaler', Display_Figure='on')   # Type_Normalize:'MinMaxScaler', 'normalize'
+# ======================================= Step 3: Feature Extraction & Selection =======================================
+# Data = feature_extraction(Data, Labels, number_feature=3, number_neighbors=70, type_feature='LDA')
+# Data = featureselection(Data, Labels, threshold=0.1, number_feature=3, c_l1fs=0.01, n_estimators_tfs=100,  type_feature='MI')
+"""
+Feature Extraction:
+PCA:Principal Component Analysis; LDA:Linear discriminant analysis; ICA: Independent component analysis; SVD: Singular value decomposition
+TSNE:T-distributed stochastic neighbor embedding; FA: Factor analysis; Isomap: Isometric Feature Mapping
+Feature Selection:
+Variance; Mutual information (MI); Chi-square test (Chi-square); fisher_score (FS); Forward feature selection (FFS);
+Backward feature selection (BFS); Exhaustive Feature Selection (EFS); Recursive feature elimination (RFE); Random Forest (RF)
+Univariate feature selection (UFS); L1-based feature selection (L1FS), Tree-based feature selection (TFS)
+"""
+# ===================================== Step 4: Classification & Clustering ==========================================
+# ----------------------------------------- Step 4: Classification ---------------------------------------------------
+model, type_class = classification(Data, Labels, type_class='XGBoost', hidden_layer_mlp=(10,), max_iter=200, kernel_svm='rbf',
+                               c_svm=10, gamma_svm=0.7, max_depth=5, criterion_dt='entropy', n_estimators=500)
+Accuracy_Train, Cr_Train, Accuracy_Test, Cr_Test = output_network(Data, Labels, model, type_class, k_fold=5)
+"""
+type_class: 'KNN', 'LR', 'MLP', 'SVM', 'DT', 'NB', 'RF', 'AdaBoost', 'XGBoost', 'LDA'
+LR: LogisticRegression; MLP: Multilayer perceptron, SVM:Support Vector Machine; DT: Decision Tree; NB: Naive Bayes;
+RF: Random Forest; AdaBoost; XGBoost; LDA: Linear Discriminant Analysis; KNN:K-Nearest Neighbors 
+Parameters:
+The number of hidden layers: hidden_layer_mlp; The number of epochs MLP: max_iter,
+kernel_svm=‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’;  c_svm=Regularization parameter, 
+gamma_svm=Kernel coefficient for ‘rbf’, ‘poly’ and ‘sigmoid’. 
+max_depth=The maximum depth of the tree, random forest and XGBoost; criterion= 'gini', 'entropy', 'log_loss';
+n_estimators:The number of trees in the forest.
+"""
+# ----------------------------------------- Step 4: Clustering -----------------------------------------------------
+clustering(Data, n_clusters=3, max_iter=100, thr_brich=0.5,  branchfactor_brich=50, n_neighbor_SpecCluster=10,
+           minsamples_optics=15, max_dist_optics=5, batch_size_MBKmeans=10, type_cluster='MiniBatchKMeans')
+"""
+type_cluster: kmeans; Agglomerative; DBSCAN; GMM:Gaussian Mixture Models; Meanshift; Birch; SpectralClustering; 
+OPTICS; MiniBatchKMeans
+"""
---
+++ Code/Plot_Roc_Curve.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sklearn import metrics
+import matplotlib.pyplot as plt
+
+
+def Plot_Roc_curve(tpr, mean_fpr, auc, ax, title):
+    mean_tpr = np.mean(tpr, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = metrics.auc(mean_fpr, mean_tpr)
+    std_auc = np.std(auc)
+    ax.plot([0, 1], [0, 1], "k--", lw=1, alpha=0.4)
+    ax.plot(mean_fpr, mean_tpr, label='Mean ROC (AUC = ' + np.str(np.round(mean_auc, 2)) + '$\pm$' +
+                                      np.str(np.round(std_auc, 3)) + ')', lw=2, alpha=.8, color='b')
+    std_tpr = np.std(tpr, axis=0)
+    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.25, label=r'$\pm$  std. dev.')
+    ax.set_xlabel("False Positive Rate")
+    ax.set_title(title)
+    ax.legend(fontsize=8)
+
+
+
