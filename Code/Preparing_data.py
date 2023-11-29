c1dfd23446ecc0ef3c19c409cb0cdf87415e2ef5
Reza Saadatyar <96347878+RezaSaadatyar@users.noreply.github.com>
2023-11-29 20:29:33 +0330

    Delete Preparing_data.py

--- Code/Preparing_data.py
+++
@@ -1,9 +0,0 @@
-from sklearn import preprocessing
-
-
-def preparing_data(data, labels):
-    if data.shape[0] < data.shape[1]:
-        data = data.T
-    Labels = preprocessing.LabelEncoder()
-    Labels = Labels.fit_transform(labels)
-    return data, Labels
