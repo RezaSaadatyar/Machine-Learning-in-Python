fcec45696f4557f469766f8f7b6d27bcd7785555
Reza Saadatyar <96347878+RezaSaadatyar@users.noreply.github.com>
2023-11-30 09:44:29 +0330

    Create Filtering.py

---
+++ Code/Funtions/Filtering.py
@@ -0,0 +1,23 @@
+from scipy import signal
+
+
+# ================================================ Filtering ====================================================
+def filtering(data, f_low, f_high, order, Fs, filter_type):
+    if data.ndim > 1:
+        if data.shape[0] > data.shape[1]:
+            data = data.T
+            
+    f_low = f_low / (Fs / 2)
+    f_high = f_high / (Fs / 2)
+    
+    if filter_type == "low":
+        b, a = signal.butter(order, f_low, btype='low')
+    elif filter_type == "high":
+        b, a = signal.butter(order, f_high, btype='high')
+    elif filter_type == "bandpass":
+        b, a = signal.butter(order, [f_low, f_high], btype='bandpass')
+    elif filter_type == "bandstop":
+        b, a = signal.butter(order, [f_low, f_high], btype='bandstop')
+    filtered_data = signal.filtfilt(b, a, data)
+    
+    return filtered_data.T
