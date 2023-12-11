e64064692871bd2bd5ff065a1804658718f66820
Reza Saadatyar <96347878+RezaSaadatyar@users.noreply.github.com>
2023-12-12 01:47:09 +0330

    Update Preparing_data.py

--- Code/Funtions/Preparing_data.py
+++ Code/Funtions/Preparing_data.py
@@ -1,9 +1,16 @@
+import numpy as np
 from sklearn import preprocessing
 
-
+# =============================================== Preparing data =========================================================
 def preparing_data(data, labels):
+    
     if data.shape[0] < data.shape[1]:
         data = data.T
-    Labels = preprocessing.LabelEncoder()
-    Labels = Labels.fit_transform(labels)
-    return data, Labels
+    
+    if len(labels.shape) > 1:
+        labels = np.max(labels) + 1  # Assuming classes are 0-indexed
+    
+    mod = preprocessing.LabelEncoder()
+    labels = mod.fit_transform(labels)
+    
+    return data, labels
