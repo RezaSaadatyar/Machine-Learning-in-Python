b90443f3e1476b9820dde85c7c42cf91b820ac97
Reza Saadatyar <96347878+RezaSaadatyar@users.noreply.github.com>
2023-12-18 19:58:40 +0330

    Update KNN_Neighbors_Optimal.py

--- Code/Funtions/KNN_Neighbors_Optimal.py
+++ Code/Funtions/KNN_Neighbors_Optimal.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn import neighbors
 
 # =================================================== KNN_optimal ========================================================  
-def knn_optimal(data_train, label_train, data_test, label_test, n=21, fig_size=(3,2), display_figure="off"):
+def knn_optimal(data_train, label_train, data_test, label_test, display_optimal_k="off", n=21, fig_size=(3.5,2.5)):
     if np.shape(data_train)[0] < np.shape(data_train)[1]:  # Convert Data training & Test >>>> m*n; m > n
         data_train = data_train.T
         data_test = data_test.T
@@ -16,7 +16,7 @@ def knn_optimal(data_train, label_train, data_test, label_test, n=21, fig_size=(
         accuracy_train[i-1] = model.score(data_train, label_train)
         accuracy_test[i-1] = model.score(data_test, label_test)
     
-    if display_figure == "on":
+    if display_optimal_k == "on":
         
         plt.figure(figsize=fig_size)
         plt.plot(t, accuracy_train, label="Training")
@@ -26,7 +26,7 @@ def knn_optimal(data_train, label_train, data_test, label_test, n=21, fig_size=(
         
         plt.xlabel("Number of neighbors")
         plt.ylabel("Accuracy")
-        plt.title("KNN", fontsize=10)
+        plt.title(f"Optimal_k for KNN: {t[np.argmax(accuracy_test)]}", fontsize=10)
         plt.tick_params(axis='x', rotation=90)
 
     return t[np.argmax(accuracy_test)]
